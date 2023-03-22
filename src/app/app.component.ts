43da5e1a804103f5cd80e67338392444d67f7c33
Phil-In-The-Blank <philip.christensen5689@gmail.com>
2023-03-22 09:41:49 -0400

    added home page, sidenav, resume base component and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  constructor(private router: Router){}
+
+  protected navigate(path: string){
+    this.router.navigate([path])
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResumeComponent,
   ],
   imports: [
+    
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule.forRoot([{
+      path: 'resume', component: ResumeComponent
+    },
+    {path: '', component: HomeComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
